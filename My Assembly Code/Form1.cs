4d85294afcfb317ea636beed13c8d2bb1c478db0
darien <darientorok1@gmail.com>
2026-02-12 00:53:06 -0600

    finish gui

--- My Assembly Code/Form1.cs
+++ My Assembly Code/Form1.cs
@@ -1,8 +1,8 @@
 namespace My_Assembly_Code
 {
-    public partial class Form1 : Form
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
         }
