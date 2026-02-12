15a1fddaaab51d62a1ecee963a49a6d0ebaca52c
darien <darientorok1@gmail.com>
2026-02-12 17:28:31 -0600

    command

---
+++ My Assembly Code/Command.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Assembly_Code
+{
+    public class Command
+    {
+        public string Operation { get; set; }
+        public string Var1 { get; set; }
+        public string Var2 { get; set; }
+        public string Var3 { get; set; }
+    }
+}
