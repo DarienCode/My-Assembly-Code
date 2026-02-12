15eb3b5386909efb0369bd99a0c4167d31696f4f
Christopher Moncur <Christopher.Moncur@southeasttech.edu>
2026-02-12 16:32:51 -0600

    Clarified comments & re-reorganized LD code into function

--- My Assembly Code/Form1.cs
+++ My Assembly Code/Form1.cs
@@ -2,26 +2,44 @@ namespace My_Assembly_Code
 {
     public partial class Form : System.Windows.Forms.Form
     {
-        int r1 = 0;
-        int r2 = 0;
-        int r3 = 0;
+        string[] user_input = {"foo"}; // String array to read input from input textbox
+        int r1 = 0; // Register 1
+        int r2 = 0; // Register 2
+        int r3 = 0; // Register 3
 
-        public void Load(int i, int reg) // LOAD function
+        public void Load() // LOAD command
         {
-            switch (reg)
+            if (int.TryParse(user_input[1], out int var) == true) // Validate the value being assigned
             {
-                case 1:
-                    r1 = i;
-                    break;
-                case 2:
-                    r2 = i;
-                    break;
-                case 3:
-                    r3 = i;
-                    break;
-                default: 
-                    break;
+                switch (user_input[2]) // Validate the register being assigned to
+                {
+                    case "R1":
+                        {
+                            r1 = var;
+                            break;
+                        }
+                    case "R2":
+                        {
+                            r2 = var;
+                            break;
+                        }
+                    case "R3":
+                        {
+                            r3 = var;
+                            break;
+                        }
+                    default: // Register error
+                        {
+                            MessageBox.Show("Must name a valid register to assign to. (R1, R2, or R3)");
+                            break;
+                        }
+                }
             }
+            else // Assigning value error
+            {
+                MessageBox.Show("Value registered must be an integer.");
+                return;
+            }           
         }
         public Form()
         {
@@ -31,43 +49,19 @@ namespace My_Assembly_Code
 
         private void output_TextChanged(object sender, EventArgs e)
         {
-
+            // We can probably ignore this
         }
 
         private void runBtn_Click(object sender, EventArgs e)
         {
-            string[] user_input = input.Text.ToUpper().Split(' '); // (chris) Read from input textbox & assign uppercase split strings to user_input
-            // Use user_input[] to read each part of user commands
-            if (user_input[0] == "LD") // LOAD command
+            user_input = input.Text.ToUpper().Split(' '); // Save textbox input to user_input, separated as an array
+            if (user_input[0] == "LD") // Call the LOAD command
             {
-                if (int.TryParse(user_input[1], out int v1) == true) // Validate the value being assigned
-                {
-                    if (user_input[2] == "R1") // Validate the register which the value is being assigned to
-                    {
-                        Load(v1,1);
-                    }
-                    else if (user_input[2] == "R2")
-                    {
-                        Load(v1,2);
-                    }
-                    else if (user_input[2] == "R3")
-                    {
-                        Load(v1,3);
-                    }
-                    else // Throw register error
-                    {
-                        MessageBox.Show("Must give a valid register to assign the value to.");
-                    }
-                }
-                else // Throw assigning value error
-                {
-                    MessageBox.Show("Cannot register an invalid value.");
-                    return;
-                }
+                Load();
             }
-            else
+            else // Command error
             {
-                MessageBox.Show("Invalid command.");
+                MessageBox.Show("'" + user_input[0] +  "' is an invalid command.");
             }
         }
     }
